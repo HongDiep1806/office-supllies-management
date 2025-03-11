3ea102ed2df4204066b60aa6953f7147c452a50e
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2025-03-11 13:45:05 +0700

    change summary model

---
+++ Office supplies management/Migrations/20250311064244_updateModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Office_supplies_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApprovedBySupLead",
+                table: "Summaries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProcessedBySupLead",
+                table: "Summaries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApprovedBySupLead",
+                table: "Summaries");
+
+            migrationBuilder.DropColumn(
+                name: "IsProcessedBySupLead",
+                table: "Summaries");
+        }
+    }
+}

153e90796d433056f5db3c7d7cfa8b2130ccb916
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2025-03-11 11:17:53 +0700

    push

--- Office supplies management/obj/Debug/net8.0/Office supplies management.AssemblyInfo.cs
+++ Office supplies management/obj/Debug/net8.0/Office supplies management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Office supplies management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8960715b224490522d11b47dfdb267b3c6f9346a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41ded28ac2d0bd64e4257cf100b0c9dee40ccfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Office supplies management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Office supplies management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
